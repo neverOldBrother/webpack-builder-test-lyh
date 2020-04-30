08e26aa40c5e650da14bbe01f1a901a9de9c34f7
neverOldBrother <810967661@qq.com>
2020-04-30 22:29:02 +0800

    travis ci

--- test/smoke/unit/webpack-base-test.js
+++ test/smoke/unit/webpack-base-test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 
 describe('webpack.base test case', ()=>{
-  const baseConfig = require('../../lib/webpack.base.js')
+  const baseConfig = require('../../../lib/webpack.base.js')
   console.log('baseConfig',baseConfig)
   it('entry', ()=>{
     assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true)
